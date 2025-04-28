0d52703b9780cc9abc6bf194d4b6967d7cb831ff
Alla Tambovtseva <allatambov@gmail.com>
2025-04-28 14:08:56 +0300

    Update temp.py

--- temp.py
+++ temp.py
@@ -2,15 +2,4 @@ import pandas as pd
 import statsmodels.api as sm
 from statsmodels.formula.api import ols
 
-### LOAD AND DESCRIBE ###
-df = pd.read_csv("https://raw.githubusercontent.com/allatambov/StatCS25/refs/heads/main/cities.csv")
-print(df.head())
-print(df.groupby("city")["height"].describe())
-
-df.hist("height", by = "city", edgecolor = "white");
-df.boxplot("height", by = "city");
-
-### ANOVA ###
-mod = ols('height ~ city', data = df).fit()
-tab = sm.stats.anova_lm(mod)
-print(tab)
+flats = pd.read_csv("https://raw.githubusercontent.com/allatambov/StatCS25/refs/heads/main/flats_cian.csv")
